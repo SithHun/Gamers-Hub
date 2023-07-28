205aded26d7abf0b086e079df64bda688044ef6a
Seth Sun <sithsun@gmail.com>
2023-07-27 20:03:13 -0400

    Require apolloserver and auth and resolvers/typedefs

--- server/server.js
+++ server/server.js
@@ -2,6 +2,9 @@ const express = require('express');
 const path = require('path');
 const db = require('./config/connection');
 const routes = require('./routes');
+const { ApolloServer } = require('apollo-server-express');
+const { typeDefs, resolvers } = require('./schemas');
+const { authMiddleware } = require('./utils/auth')
 
 const app = express();
 const PORT = process.env.PORT || 3001;
