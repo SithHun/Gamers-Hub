15cfc4fcdcdce897bea41fd659f1e2550f32861d
Seth Sun <sithsun@gmail.com>
2023-07-27 20:04:12 -0400

    Start apolloserver with DB.

--- server/server.js
+++ server/server.js
@@ -24,6 +24,16 @@ if (process.env.NODE_ENV === 'production') {
 
 app.use(routes);
 
-db.once('open', () => {
-  app.listen(PORT, () => console.log(`Now listening on localhost:${PORT}`));
-});
+const startApolloServer = async () => {
+  await server.start();
+  server.applyMiddleware({ app });
+
+  db.once('open', () => {
+    app.listen(PORT, () => {
+      console.log(`API server running on port ${PORT}!`);
+      console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+    })
+  })
+};
+
+startApolloServer();
