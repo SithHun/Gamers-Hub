b009b21ed7418150ddfd0edd361464ca31e24f13
Seth Sun <sithsun@gmail.com>
2023-07-27 20:03:40 -0400

    Added apolloserver with authmiddleware

--- server/server.js
+++ server/server.js
@@ -8,6 +8,11 @@ const { authMiddleware } = require('./utils/auth')
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware
+});
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
