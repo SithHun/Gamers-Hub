2e207d7441cb1fe80eb4709b3d9d75fca25902b1
Seth Sun <sithsun@gmail.com>
2023-07-29 10:45:38 -0400

    Require typedefs and resolvers

--- server/server.js
+++ server/server.js
@@ -3,3 +3,5 @@ const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
 const db = require('./config/connection');
 const { authMiddleware } = require('./utils/auth');
+
+const { typeDefs, resolvers } = require('./schemas');
