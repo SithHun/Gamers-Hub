349e097378cca406ba65449f1bba7c6807dc9ec3
Seth Sun <sithsun@gmail.com>
2023-07-29 11:15:30 -0400

    Added mutations into resolvers for finding a user and updating saved data and removing data and creating user and login.

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -15,3 +15,65 @@ const resolvers = {
   
         throw new AuthenticationError('Not logged in');
       },
+
+    },
+
+    Mutation: {
+      createUser: async (parent, { username, email, password }) => {
+        const user = await User.create({ username, email, password });
+        const token = signToken(user);
+  
+        return { token, user };
+      },
+  
+      login: async (parent, { email, password }) => {
+        const user = await User.findOne({ email });
+  
+        if (!user) {
+          throw new AuthenticationError('Incorrect credentials');
+        }
+  
+        const correctPw = await user.isCorrectPassword(password);
+  
+        if (!correctPw) {
+          throw new AuthenticationError('Incorrect credentials');
+        }
+        console.log(user)
+  
+        const token = signToken(user);
+        console.log(token)
+        return { token, user };
+      },
+  
+      saveBook: async (parent, { bookData }, context) => {
+        if (context.user) {
+          const updatedUser = await User.findOneAndUpdate(
+            { _id: context.user._id },
+            { $push: { savedBooks: bookData } },
+            { new: true }
+          );
+  
+          return updatedUser;
+        }
+  
+        throw new AuthenticationError('You need to be logged in!');
+      },
+  
+      removeBook: async (parent, { bookId }, context) => {
+        if (context.user) {
+          const updatedUser = await User.findOneAndUpdate(
+            { _id: context.user._id },
+            { $pull: { savedBooks: { bookId } } },
+            { new: true }
+          );
+  
+          return updatedUser;
+        }
+  
+        throw new AuthenticationError('You need to be logged in!');
+      },
+    },
+  };
+  
+  module.exports = resolvers;
+  
