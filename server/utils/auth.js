a061ee56e6563db9af895a601cf6e4d43847b960
Seth Sun <sithsun@gmail.com>
2023-07-29 10:50:46 -0400

    coding jwt to context/req

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,14 +1,12 @@
 const jwt = require('jsonwebtoken');
 
-// set token secret and expiration date
 const secret = 'mysecretsshhhhh';
 const expiration = '2h';
 
 module.exports = {
-  // function for our authenticated routes
-  authMiddleware: function (req, res, next) {
+  authMiddleware: context => {
     // allows token to be sent via  req.query or headers
-    let token = req.query.token || req.headers.authorization;
+    let token = req.body.token || req.query.token || req.headers.authorization;
 
     // ["Bearer", "<tokenvalue>"]
     if (req.headers.authorization) {
