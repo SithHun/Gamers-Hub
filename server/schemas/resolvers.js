c98dfde2b92478b2d47e69757f09a62ef245ec50
Seth Sun <sithsun@gmail.com>
2023-07-29 11:13:43 -0400

    Resolvers for finding a user and populating the data

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,3 +1,17 @@
 const { User, Book } = require('../models');
 const { AuthenticationError } = require('apollo-server-express');
 const { signToken } = require('../utils/auth');
+
+const resolvers = {
+    Query: {
+      me: async (parent, args, context) => {
+        if (context.user) {
+          const userData = await User.findOne({ _id: context.user._id })
+            .select('-__v -password')
+            .populate('savedBooks');
+  
+          return userData;
+        }
+  
+        throw new AuthenticationError('Not logged in');
+      },
