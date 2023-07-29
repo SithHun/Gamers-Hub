3f70fd62236e4bf8f03e08391fcfa408e69da3a3
Seth Sun <sithsun@gmail.com>
2023-07-29 10:46:41 -0400

    Coded start server with apollo and express and create path to graphql

--- server/server.js
+++ server/server.js
@@ -5,3 +5,32 @@ const db = require('./config/connection');
 const { authMiddleware } = require('./utils/auth');
 
 const { typeDefs, resolvers } = require('./schemas');
+
+const startServer = async () => {
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    context: authMiddleware,
+  });
+
+  await server.start();
+
+  const app = express();
+  server.applyMiddleware({ app, path: '/graphql' });
+
+  app.use(express.urlencoded({ extended: false }));
+  app.use(express.json());
+
+  if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/build')));
+  }
+
+  db.once('open', () => {
+    app.listen(process.env.PORT || 3001, () => {
+      console.log(`API server running on port ${process.env.PORT || 3001}!`);
+      console.log(`Use GraphQL at http://localhost:${process.env.PORT || 3001}${server.graphqlPath}`);
+    });
+  });
+};
+
+startServer();
