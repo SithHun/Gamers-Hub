3f080ea59e66fa09cc03d0a1e48fbb7bb99ea8a0
Seth Sun <sithsun@gmail.com>
2023-07-30 15:49:03 -0400

    Changed how fetch handles and api for rawg

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,5 +1,12 @@
-// make a search to google books api
-// https://www.googleapis.com/books/v1/volumes?q=harry+potter
-export const searchGoogleBooks = (query) => {
-  return fetch(`https://www.googleapis.com/books/v1/volumes?q=${query}`);
+// const RAWG_API_KEY =  process.env.REACT_APP_RAWG_API_KEY;
+const RAWG_API_KEY = '2ce1ade5c6ce4ac487c93348d52fab6f';
+const RAWG_API_URL = 'https://api.rawg.io/api/games';
+// require('dotenv').config();
+
+export const searchRAWGGames = (query) => {
+  // Construct the URL for the RAWG API.
+  const url = `${RAWG_API_URL}?key=${RAWG_API_KEY}&search=${query}`;
+
+  // Use fetch to get the data from the RAWG API.
+  return fetch(url);
 };
