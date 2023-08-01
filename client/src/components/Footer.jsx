54a1a97d00a9f3a8c55ed985fbcc2548bcd8fb41
Benjamin Bryant <bryantbc98@gmail.com>
2023-07-31 20:10:33 -0400

    updated footer for home page

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,40 +1,31 @@
 import React from "react";
+
 export default function SiteFooter() {
   const openNewTab = (url) => {
     window.open(url, "_blank", "noreferrer");
   };
   return (
     <div className="container footer bg-slate">
-      <footer
-        className="text-center text-lg-start"
-      >
+      <footer className="text-center text-lg-start">
         <div className="container d-flex justify-content-center py-5">
-          <button
+          <img
+            className="btn-footer mx-4"
+            src="https://1000logos.net/wp-content/uploads/2021/05/GitHub-logo.png"
+          ></img>
+          <img
             onClick={() => openNewTab("https://facebook.com")}
-            type="button"
-            className="btn btn-danger btn-lg btn-floating mx-2"
-          >
-            <i className="fab fa-facebook-f"></i>
-          </button>
-          <button
-            type="button"
-            className="btn btn-primary btn-lg btn-floating mx-2"
-          >
-            <i className="fab fa-youtube"></i>
-          </button>
-          <button
-            type="button"
-            className="btn btn-primary btn-lg btn-floating mx-2"
-          >
-            <i className="fab fa-instagram"></i>
-          </button>
-          <button
-            type="button"
-            className="btn btn-primary btn-lg btn-floating mx-2"
-          >
-            <i className="fab fa-twitter"></i>
-          </button>
+            src="https://upload.wikimedia.org/wikipedia/commons/1/1b/Facebook_icon.svg"
+            className="btn-footer mx-4"
+          ></img>
+          <img
+            onClick={() => openNewTab("https://twitter.com")}
+            className="btn-footer mx-4"
+            src="https://upload.wikimedia.org/wikipedia/commons/6/6f/Logo_of_Twitter.svg"
+          ></img>
         </div>
+        <h6 className="text-center text-light">
+          Gamer's Hub 2023 | Developed by{" "}
+        </h6>
       </footer>
     </div>
   );
