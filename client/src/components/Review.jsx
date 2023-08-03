449b49ab7f15bc63b89f17086c56bf56d2bae8c2
Benjamin Bryant <bryantbc98@gmail.com>
2023-08-02 23:29:08 -0400

    added webfont package and changed styling and layout of review cards

--- client/src/components/Review.jsx
+++ client/src/components/Review.jsx
@@ -1,19 +1,33 @@
-import React from "react";
+import React, { useEffect } from "react";
+import WebFont from "webfontloader";
 
 export default function Review(props) {
-    return (
-        <div className="reviewCard">
-      <h4 className="quote">"<span className="title">{props.title} </span>"</h4>
+  useEffect(() => {
+    WebFont.load({
+      google: {
+        families: ["Lumanosimo"],
+      },
+    });
+  });
+  return (
+    <div className="reviewCard">
+      <h4 className="quote">
+        "<span className="title">{props.title} </span>"
+      </h4>
       <div className="flexWrapper gamePanel">
         <img className="thumbnail" src={props.image}></img>
         <h5 className="game">{props.game}</h5>
-        <h5 className="reviewRating">{props.rating}<span className="outOf">/100</span></h5>
+        <h5 className="platform">{props.platform}</h5>
+        <div className="reviewRating">
+          <h5 className="ratingText">{props.rating}</h5>
+          <h5 className="outOf">/100</h5>
+        </div>
       </div>
       <div className="flexWrapper">
         <h6 className="reviewBody">
           {props.body}
           <span className="elipse">
-        ...<span className="readMore">Read More</span>
+            ...<span className="readMore">Read More</span>
           </span>
         </h6>
       </div>
--- client/src/components/Reviews.jsx
+++ client/src/components/Reviews.jsx
@@ -5,22 +5,24 @@ const reviewsArr = [
   {
     image:
       "https://www.giantbomb.com/a/uploads/scale_small/41/414731/3094375-emberfallboxart.jpg",
-    title: "An Epic Journey through a Rich Fantasy World",
+    title: "An Epic Journey through Fantasy",
     author: "Ned",
     game: "Emberfall",
     rating: 94,
     body: "Emberfall is an absolute gem of a video game. From the moment I stepped foot into the beautifully crafted world, to the first time I discovered",
     date: "June 12, 2017",
+    platform: "PC",
   },
   {
     image:
       "https://cdn.cloudflare.steamstatic.com/steam/apps/1555930/capsule_616x353.jpg?t=1663965515",
-    title: "An Emotional Journey Through Fantastical Worlds",
+    title: "Exploration at it's Cutest",
     author: "Jocinda",
     game: "Beyond Horizon",
     rating: 95,
     body: "Beyond Horizon is a beautifully crafted 2D platformer that delivers a heartfelt story and captivating gameplay. Players join a young explorer",
     date: "April 13, 2022",
+    platform: "PC",
   },
   {
     image:
@@ -31,16 +33,18 @@ const reviewsArr = [
     rating: 84,
     body: "CyberPunk 2077 offers a mesmerizing journey into a dystopian future. The game's broody aesthetic and pulsating realism delivers a frenzy of",
     date: "December 2, 2021",
+    platform: "PS4",
   },
   {
     image:
       "https://cdn.cloudflare.steamstatic.com/steam/apps/2452900/capsule_616x353.jpg?t=1689248961",
-    title: "Unleash Your Creativity in a World of Wonders",
+    title: "Unleash Your Creativity",
     author: "Patricia",
     game: "TerraForge: Reborn",
     rating: 89,
     body: "TerraForge: Reborn is a sandbox masterpiece that grants you the power of creation. The freedom to shape and mold the world as you please is",
     date: "August 8, 2020",
+    platform: "PC",
   },
 ];
 
@@ -62,6 +66,7 @@ export default function Reviews() {
               game={review.game}
               body={review.body}
               date={review.date}
+              platform={review.platform}
             />
           );
         })}
