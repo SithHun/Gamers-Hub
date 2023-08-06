fbdcad138dbb58b74fa25c1a76a3a15bf2f8553f
Seth Sun <sithsun@gmail.com>
2023-08-05 21:50:31 -0400

    styling for savedgames, discussion added edit and delete

--- client/src/pages/SavedGames.js
+++ client/src/pages/SavedGames.js
@@ -5,6 +5,8 @@ import { QUERY_ME, QUERY_DISCUSSIONS } from '../utils/queries';
 import { REMOVE_GAME, ADD_DISCUSSION } from '../utils/mutations';
 import Auth from '../utils/auth';
 import { removeGameId } from '../utils/localStorage';
+import { EDIT_DISCUSSION, DELETE_DISCUSSION } from '../utils/mutations';
+
 
 const SavedGames = () => {
   const { loading, data } = useQuery(QUERY_ME);
@@ -12,6 +14,8 @@ const SavedGames = () => {
   const [selectedGame, setSelectedGame] = useState(null);
   const [show, setShow] = useState(false);
   const [newDiscussion, setNewDiscussion] = useState("");
+  const [editingDiscussion, setEditingDiscussion] = useState(null);
+  const [updatedDiscussionBody, setUpdatedDiscussionBody] = useState("");
 
   const {
     loading: loadingDiscussions,
@@ -22,8 +26,12 @@ const SavedGames = () => {
     skip: !selectedGame,
   });
 
+  // hooks for mutations
   const [removeGame] = useMutation(REMOVE_GAME);
   const [addDiscussion] = useMutation(ADD_DISCUSSION);
+  const [editDiscussion] = useMutation(EDIT_DISCUSSION);
+  const [deleteDiscussion] = useMutation(DELETE_DISCUSSION);
+
 
   useEffect(() => {
     if (data) {
@@ -82,6 +90,35 @@ const SavedGames = () => {
     }
   };
 
+  const handleDeleteDiscussion = async (discussionId, gameId) => {
+    try {
+      await deleteDiscussion({
+        variables: { userId: userData._id, gameId },
+      });
+      // Refetch discussions to immediately reflect the changes in the UI.
+      refetchDiscussions();
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  const handleEditDiscussion = async () => {
+    if (editingDiscussion && updatedDiscussionBody) {
+      try {
+        await editDiscussion({
+          variables: { userId: userData._id, gameId: selectedGame, body: updatedDiscussionBody },
+        });
+        // Reset the states and refetch discussions.
+        setEditingDiscussion(null);
+        setUpdatedDiscussionBody("");
+        refetchDiscussions();
+      } catch (err) {
+        console.error(err);
+      }
+    }
+  };
+
+
   if (loading) {
     return <h2>LOADING...</h2>;
   }
@@ -124,45 +161,79 @@ const SavedGames = () => {
           })}
         </Row>
       </Container>
-
+  
       {/* Discussion modal */}
-      <Modal show={show} onHide={handleClose} centered>
+      <Modal show={show} onHide={handleClose} centered dialogClassName="custom-modal" size='lg'>
         <Modal.Header closeButton>
           <Modal.Title>Discussions</Modal.Title>
         </Modal.Header>
-        <Modal.Body>
+        <Modal.Body style={{ margin: "10px" }}>
           {!loadingDiscussions && discussionsData && discussionsData.discussions.length > 0
             ? discussionsData.discussions.map((discussion) => (
-                <p key={discussion._id}>
-                  <strong>{discussion.userId.username}</strong>: {discussion.body}
-                </p>
+                <div key={discussion._id}>
+                  <p>
+                    <strong>{discussion.userId.username}</strong>: {discussion.body}
+                  </p>
+                  {editingDiscussion === discussion._id ? (
+                    <div>
+                      <Form.Control
+                        type="text"
+                        value={updatedDiscussionBody}
+                        onChange={(e) => setUpdatedDiscussionBody(e.target.value)}
+                        placeholder="Update your discussion here..."
+                      />
+                      <Button onClick={handleEditDiscussion}>Apply changes</Button>
+                      <Button onClick={() => setEditingDiscussion(null)}>Cancel</Button>
+                    </div>
+                  ) : (
+                    <div>
+                      <Button onClick={() => {
+                        setEditingDiscussion(discussion._id);
+                        setUpdatedDiscussionBody(discussion.body);
+                      }}>
+                        Edit
+                      </Button>
+                      <Button onClick={() => handleDeleteDiscussion(discussion._id, discussion.gameId)}>
+                        Delete
+                      </Button>
+                    </div>
+                  )}
+                </div>
               ))
             : "No discussions for this game yet."}
-
+  
           {/* Add discussion form */}
-          <Form onSubmit={handleAddDiscussion}>
+          <Form onSubmit={handleAddDiscussion} style={{ padding: '1em' }}>
+          <Row className="align-items-center">
+          <Col>
             <Form.Group>
-              <Form.Label>Add a Discussion</Form.Label>
+              {/* <Form.Label>Add a Discussion</Form.Label> */}
               <Form.Control
                 type="text"
                 value={newDiscussion}
                 onChange={(e) => setNewDiscussion(e.target.value)}
                 placeholder="Write your discussion here..."
+                style={{ marginTop: "35px" }}
               />
             </Form.Group>
-            <Button variant="primary" type="submit">
+            </Col>
+            <Col xs="auto">
+            <Button variant="primary" type="submit" style={{ color: 'azure', backgroundColor: 'teal', borderColor: 'teal', marginTop: "35px" }}>
               Submit
             </Button>
+            </Col>
+            </Row>
           </Form>
         </Modal.Body>
         <Modal.Footer>
-          <Button variant="secondary" onClick={handleClose}>
+          <Button variant="secondary" onClick={handleClose} style={{ color: 'azure', backgroundColor: 'teal', borderColor: 'teal'}}>
             Close
           </Button>
         </Modal.Footer>
       </Modal>
     </>
   );
+  
 };
 
 export default SavedGames;
