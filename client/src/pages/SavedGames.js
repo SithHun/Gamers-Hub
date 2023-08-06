8aa917362de8734f94dc0fec1286ecdb2949e1c4
Benjamin Bryant <bryantbc98@gmail.com>
2023-08-06 02:27:11 -0400

    merged changes from main

--- client/src/pages/SavedGames.js
+++ client/src/pages/SavedGames.js
@@ -1,10 +1,19 @@
-import React, { useState, useEffect } from 'react';
-import { Container, Card, Button, Row, Col, Modal, Form } from 'react-bootstrap';
-import { useQuery, useMutation } from '@apollo/client';
-import { QUERY_ME, QUERY_DISCUSSIONS } from '../utils/queries';
-import { REMOVE_GAME, ADD_DISCUSSION } from '../utils/mutations';
-import Auth from '../utils/auth';
-import { removeGameId } from '../utils/localStorage';
+import React, { useState, useEffect } from "react";
+import {
+  Container,
+  Card,
+  Button,
+  Row,
+  Col,
+  Modal,
+  Form,
+} from "react-bootstrap";
+import { useQuery, useMutation } from "@apollo/client";
+import { QUERY_ME, QUERY_DISCUSSIONS } from "../utils/queries";
+import { REMOVE_GAME, ADD_DISCUSSION, DELETE_DISCUSSION, EDIT_DISCUSSION } from "../utils/mutations";
+import Auth from "../utils/auth";
+import { removeGameId } from "../utils/localStorage";
+
 
 const SavedGames = () => {
   const { loading, data } = useQuery(QUERY_ME);
@@ -12,6 +21,8 @@ const SavedGames = () => {
   const [selectedGame, setSelectedGame] = useState(null);
   const [show, setShow] = useState(false);
   const [newDiscussion, setNewDiscussion] = useState("");
+  const [editingDiscussion, setEditingDiscussion] = useState(null);
+  const [updatedDiscussionBody, setUpdatedDiscussionBody] = useState("");
 
   const {
     loading: loadingDiscussions,
@@ -22,8 +33,12 @@ const SavedGames = () => {
     skip: !selectedGame,
   });
 
+  // hooks for mutations
   const [removeGame] = useMutation(REMOVE_GAME);
   const [addDiscussion] = useMutation(ADD_DISCUSSION);
+  const [editDiscussion] = useMutation(EDIT_DISCUSSION);
+  const [deleteDiscussion] = useMutation(DELETE_DISCUSSION);
+
 
   useEffect(() => {
     if (data) {
@@ -82,6 +97,35 @@ const SavedGames = () => {
     }
   };
 
+  const handleDeleteDiscussion = async (discussionId, gameId) => {
+    try {
+      await deleteDiscussion({
+        variables: { userId: userData._id, gameId },
+      });
+      // Refetch discussions to immediately reflect the changes in the UI.
+      refetchDiscussions();
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  const handleEditDiscussion = async () => {
+    if (editingDiscussion && updatedDiscussionBody) {
+      try {
+        await editDiscussion({
+          variables: { userId: userData._id, gameId: selectedGame, body: updatedDiscussionBody },
+        });
+        // Reset the states and refetch discussions.
+        setEditingDiscussion(null);
+        setUpdatedDiscussionBody("");
+        refetchDiscussions();
+      } catch (err) {
+        console.error(err);
+      }
+    }
+  };
+
+
   if (loading) {
     return <h2>LOADING...</h2>;
   }
@@ -95,28 +139,32 @@ const SavedGames = () => {
         <h3>{userData.username}</h3>
       </Container>
       <Container>
-        <h2 className="pt-5">
+        <br></br>
+        <br></br>
+        {/* <h2 className="pt-5">
           {savedGames.length
             ? `${savedGames.length} added ${savedGames.length === 1 ? 'collection' : 'collections'}:`
             : 'You have no saved collection!'}
-        </h2>
-        <Row>
+        </h2> */}
+        <Row className="game-card-row">
           {savedGames.map((game) => {
             return (
               <Col md="4" key={game.gameId}>
-                <Card border="dark">
+                <Card border="dark" className="game-card hover-effect">
                   {game.image && (
                     <Card.Img src={game.image} alt={`The cover for ${game.title}`} variant="top" />
                   )}
                   <Card.Body>
                     <Card.Title>{game.title}</Card.Title>
-                    <Card.Text>{game.description}</Card.Text>
-                    <Button className="btn-block btn-danger" onClick={() => handleDeleteGame(game.gameId)}>
-                      Delete this Game!
+                    {/* <Card.Text>{game.description}</Card.Text> */}
+                    <Button className="btn-block btn-danger delete-button show-discussions-button hoverButton" onClick={() => handleDeleteGame(game.gameId)}>
+                      X
                     </Button>
-                    <Button variant="primary" onClick={() => handleShow(game.gameId)}>
+                    <div className="absolute-button-wrapper">
+                    <Button variant="primary" className="hoverButton" style={{ margin: "10px" }} onClick={() => handleShow(game.gameId)}>
                       Show Discussions
                     </Button>
+                    </div>
                   </Card.Body>
                 </Card>
               </Col>
@@ -124,45 +172,79 @@ const SavedGames = () => {
           })}
         </Row>
       </Container>
-
+  
       {/* Discussion modal */}
-      <Modal show={show} onHide={handleClose} centered>
+      <Modal show={show} onHide={handleClose} centered dialogClassName="custom-modal" size='lg'>
         <Modal.Header closeButton>
           <Modal.Title>Discussions</Modal.Title>
         </Modal.Header>
-        <Modal.Body>
+        <Modal.Body style={{ margin: "10px" }}>
           {!loadingDiscussions && discussionsData && discussionsData.discussions.length > 0
             ? discussionsData.discussions.map((discussion) => (
-                <p key={discussion._id}>
-                  <strong>{discussion.userId.username}</strong>: {discussion.body}
-                </p>
+                <div key={discussion._id} className="discussion-item">
+                  <p>
+                    <strong>{discussion.userId.username}</strong>: {discussion.body}
+                  </p>
+                  {editingDiscussion === discussion._id ? (
+                    <div>
+                      <Form.Control
+                        type="text"
+                        value={updatedDiscussionBody}
+                        onChange={(e) => setUpdatedDiscussionBody(e.target.value)}
+                        placeholder="Update your discussion here..."
+                      />
+                      <Button className="button-spacing hoverButton" style={{ color: 'azure', backgroundColor: 'darkgray', borderColor: 'gray' }} onClick={handleEditDiscussion}>Apply changes</Button>
+                      <Button onClick={() => setEditingDiscussion(null)}>Cancel</Button>
+                    </div>
+                  ) : (
+                    <div>
+                      <Button className="button-spacing hoverButton" style={{ color: 'azure', backgroundColor: 'darkgray', borderColor: 'gray' }} onClick={() => {
+                        setEditingDiscussion(discussion._id);
+                        setUpdatedDiscussionBody(discussion.body);
+                      }}>
+                        Edit
+                      </Button>
+                      <Button className="hoverButton" style={{ color: 'black', backgroundColor: 'darkgray', borderColor: 'gray' }} onClick={() => handleDeleteDiscussion(discussion._id, discussion.gameId)}>
+                        Delete
+                      </Button>
+                    </div>
+                  )}
+                </div>
               ))
             : "No discussions for this game yet."}
-
+  
           {/* Add discussion form */}
-          <Form onSubmit={handleAddDiscussion}>
+          <Form onSubmit={handleAddDiscussion} style={{ padding: '1em' }}>
+          <Row className="align-items-center">
+          <Col>
             <Form.Group>
-              <Form.Label>Add a Discussion</Form.Label>
+              {/* <Form.Label>Add a Discussion</Form.Label> */}
               <Form.Control
                 type="text"
                 value={newDiscussion}
                 onChange={(e) => setNewDiscussion(e.target.value)}
                 placeholder="Write your discussion here..."
+                style={{ marginTop: "35px" }}
               />
             </Form.Group>
-            <Button variant="primary" type="submit">
+            </Col>
+            <Col xs="auto">
+            <Button className="hoverButton" variant="primary" type="submit" style={{ color: 'black', backgroundColor: 'darkgray', borderColor: 'gray', marginTop: "35px" }}>
               Submit
             </Button>
+            </Col>
+            </Row>
           </Form>
         </Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={handleClose}>
+        {/* <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose} style={{ color: 'azure', backgroundColor: 'teal', borderColor: 'teal'}}>
             Close
           </Button>
-        </Modal.Footer>
+        </Modal.Footer> */}
       </Modal>
     </>
   );
+  
 };
 
 export default SavedGames;
