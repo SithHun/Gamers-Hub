356df197ecfd518c190cbe18b8ed6e288805415e
Seth Sun <sithsun@gmail.com>
2023-08-06 17:55:43 -0400

    changed size of icons, slightly change size of navlinks, sticky footer to bottom of page

--- server/server.js
+++ server/server.js
@@ -23,6 +23,10 @@ const startServer = async () => {
 
   if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '../client/build')));
+
+    app.get("*", (req, res) => {
+      res.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
+    });
   }
 
   db.once('open', () => {
